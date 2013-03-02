7833a2f74b5eff46886b0990dc69b2b6d4f932c1
olivier-o <olivier@alpha2real.com>
2013-03-01 17:37:59 -0800

    Update tmux-iterm2.rb

--- tmux-iterm2.rb
+++ tmux-iterm2.rb
@@ -15,7 +15,7 @@ class TmuxIterm2 < Formula
   end
  
   def install
-    system "tar -xvf tmux-for-iTerm2-20130210.tar.gz"
+    system "tar -xvf https://code.google.com/p/iterm2/downloads/detail?name=tmux-for-iTerm2-20130210.tar.gz"
     
     Dir.chdir "tmux" do
       system "sh", "autogen.sh" if build.head?
